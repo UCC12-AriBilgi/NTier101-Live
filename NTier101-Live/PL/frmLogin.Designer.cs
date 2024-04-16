8053bc3ce138b21a78c4cd6dff6a98f0b84d5824
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-16 14:34:31 +0300

    Some changes

---
+++ NTier101-Live/PL/frmLogin.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace NTier101_Live.PL
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tboxUserName = new System.Windows.Forms.TextBox();
+            this.tboxUserPassw = new System.Windows.Forms.TextBox();
+            this.btonLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kullanıcı Adı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kullanıcı Şifre";
+            // 
+            // tboxUserName
+            // 
+            this.tboxUserName.Location = new System.Drawing.Point(93, 19);
+            this.tboxUserName.MaxLength = 10;
+            this.tboxUserName.Name = "tboxUserName";
+            this.tboxUserName.Size = new System.Drawing.Size(100, 20);
+            this.tboxUserName.TabIndex = 2;
+            // 
+            // tboxUserPassw
+            // 
+            this.tboxUserPassw.Location = new System.Drawing.Point(93, 48);
+            this.tboxUserPassw.MaxLength = 10;
+            this.tboxUserPassw.Name = "tboxUserPassw";
+            this.tboxUserPassw.Size = new System.Drawing.Size(100, 20);
+            this.tboxUserPassw.TabIndex = 3;
+            // 
+            // btonLogin
+            // 
+            this.btonLogin.Location = new System.Drawing.Point(109, 95);
+            this.btonLogin.Name = "btonLogin";
+            this.btonLogin.Size = new System.Drawing.Size(75, 23);
+            this.btonLogin.TabIndex = 4;
+            this.btonLogin.Text = "Giriş";
+            this.btonLogin.UseVisualStyleBackColor = true;
+            this.btonLogin.Click += new System.EventHandler(this.btonLogin_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 150);
+            this.Controls.Add(this.btonLogin);
+            this.Controls.Add(this.tboxUserPassw);
+            this.Controls.Add(this.tboxUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sistem Giriş";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tboxUserName;
+        private System.Windows.Forms.TextBox tboxUserPassw;
+        private System.Windows.Forms.Button btonLogin;
+    }
+}
---
+++ NTier101-Live/PL/frmLogin.cs
@@ -0,0 +1,47 @@
+﻿using NTier101_Live.BLL;
+using NTier101_Live.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NTier101_Live.PL
+{
+    public partial class frmLogin : Form
+    {
+        datUserBLL datUserBLL = new datUserBLL();
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btonLogin_Click(object sender, EventArgs e)
+        {
+            datUser datUser = new datUser();
+
+            datUser = datUserBLL.getQuery(tboxUserName.Text, tboxUserPassw.Text);
+
+            if (datUser.UserName == null || datUser.UserPassw == null)
+            {
+                MessageBox.Show("Hatalı giriş yaptınız. Lütfen kontrol ediniz...");
+            }
+            else
+            {
+                MessageBox.Show("Doğru giriş yaptınız... ");
+            }
+
+
+        }
+    }
+}
