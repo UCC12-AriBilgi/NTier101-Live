d90b2de0debd9befabaf5ff1976616aec4c3ae40
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-16 10:40:22 +0300

    Add project files.

---
+++ NTier101-Live/DBOperation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier101_Live
+{
+    internal class DBOperation
+    {
+        private SqlDataAdapter dataAdapter;
+        public SqlConnection Connection { get; set; }
+
+        // constructor
+        public DBOperation()
+        {
+            dataAdapter = new SqlDataAdapter();
+            
+            Connection = new SqlConnection(@"Data Source=EGITMEN\PC;Initial Catalog=NTier101DB;Integrated Security=True;TrustServerCertificate=True;");
+        }
+
+
+
+
+    }
+}
---
+++ NTier101-Live/Models/datUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NTier101_Live.Models
+{
+    public class datUser
+    {
+        public int UserID { get; set; }
+        public string UserName { get; set; }
+        public string UserPassw { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set;}
+        public string TCKimlik { get; set;}
+        public int MudurlukID { get; set; }
+
+    }
+}
